cdd1193ab0041ef27db2ca8e2fa7e1416b346bac
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-16 23:13:01 +0200

    task comments

--- exercises/01/01.js
+++ exercises/01/01.js
@@ -1,3 +1,6 @@
+// Pakeisti tekstini turini h1 elemente i "Labas vakaras!"
 document.querySelector('h1').innerText = `Labas vakaras!`;
+// Surasti paragrafa ir pakeisti tekstą į "Šiandien gražus oras!"
 document.querySelector('p').innerText = `Šiandien gražus oras!`;
+// Surasti nuorodą ir pakeisti tekstą į "Spausk mane!"
 document.querySelector('a').innerText = `Spausk mane!`;
--- exercises/02/02.js
+++ exercises/02/02.js
@@ -1,7 +1,14 @@
 const listDOM = document.querySelectorAll('li')
 
+// Surasti visus ingredietus ir ju pavadinimus atspausdinti i console
+
 for (let item of listDOM) {
     console.log(item.innerText);
 }
+
+/* 
+Visu ingredienu pavadinimai turi buti didziosiomis raidemis ir sudeti i array
+Isspausdinti gauta array i console
+ */
 
 console.log([...listDOM].map(a => a.innerText.toLocaleUpperCase()));
