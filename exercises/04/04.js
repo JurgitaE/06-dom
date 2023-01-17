2d2bde62fdd493624962c671c0ce1c2fe220e63a
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-17 10:20:50 +0200

    04 task corrected

--- exercises/04/04.js
+++ exercises/04/04.js
@@ -3,6 +3,6 @@ const h1DOM = document.querySelector('h1');
 // Perrasyti h1 elemento teksta i "Zuikio darzas"
 h1DOM.textContent = "Zuikio darzas";
 // Surasti visus darzoves
-const listDOM = document.querySelectorAll('li');
+const listDOM = [...document.querySelectorAll('li')].slice(0, 3);
 // Kiekvienos darzoves teksta pakeisti i "Israuta darzove"
-[...listDOM].forEach(a => a.textContent = 'Israuta darzove');
+listDOM.forEach(a => a.textContent = 'Israuta darzove');
