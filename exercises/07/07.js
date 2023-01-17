1fe5efc54a6e62c6614f0b718c7cb0c82be2715d
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-17 10:38:32 +0200

    07 task

--- exercises/07/07.js
+++ exercises/07/07.js
@@ -1,14 +1,25 @@
 
 // Susirasti minuso mygtuka
-
+const minusDOM = document.querySelector('.minus');
 // Susirasti pliuso mygtuka
-
+const plusDOM = document.querySelector('.plus');
 // Susirasti reset mygtuka
-
+const resetDOM = document.querySelector('.reset');
 // Susirasti rezultato atvaizdavimo elementa
-
+const resultDOM = document.querySelector('.number')
 // Paspaudus ant minuso, rezultatas mazinamas vienu vienetu
-
+let result = 0;
+minusDOM.addEventListener('click', () => {
+    --result;
+    resultDOM.innerText = result;
+});
 // Paspaudus ant pliuso, rezultatas didinamas vienu vienetu
-
+plusDOM.addEventListener('click', () => {
+    ++result;
+    resultDOM.innerText = result;
+});
 // Paspaudus ant reset, rezultatas turi grizti i nuli
+resetDOM.addEventListener('click', () => {
+    result = 0;
+    resultDOM.innerText = result;
+})
