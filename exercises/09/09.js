a75e5186bdb236c4a7988a0423260b5096eb72a6
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-17 11:29:02 +0200

    09 corrected

--- exercises/09/09.js
+++ exercises/09/09.js
@@ -3,6 +3,7 @@ const minusDOM = document.querySelector('.minus');
 const plusDOM = document.querySelector('.plus');
 const resetDOM = document.querySelector('.reset');
 const resultDOM = document.querySelector('.number');
+const h1DOM = document.querySelector('h1');
 // Susirasti zinuciu rasymo elementa(ul)
 
 const listDOM = document.querySelector('ul')
@@ -13,6 +14,7 @@ minusDOM.addEventListener('click', () => {
     --result;
     resultDOM.textContent = result;
     listDOM.innerHTML += `<li>Paspaudei minusa ir dabartinis rezultatas yra ${result}.</li>`
+    h1DOM.innerText = 'Žaidimas progrese';
 })
 
 // Kiekviena karta spaudziant pliuso mygtuka, reikia, jog zinuciu sarase atsirastu irasas su tekstu "Paspaudei pliusa ir dabartinis rezultatas yra [skaicius]."
@@ -20,10 +22,12 @@ plusDOM.addEventListener('click', () => {
     ++result;
     resultDOM.textContent = result;
     listDOM.innerHTML += `<li>Paspaudei pliusa ir dabartinis rezultatas yra ${result}.</li>`
+    h1DOM.innerText = 'Žaidimas progrese';
 })
 // Kiekviena karta spaudziant reset mygtuka, reikia, jog zinuciu sarase atsirastu irasas su tekstu "Paspaudei reset ir dabartinis rezultatas yra [skaicius]."
 resetDOM.addEventListener('click', () => {
     result = 0;
     resultDOM.textContent = result;
     listDOM.innerHTML += `<li>Paspaudei reset ir dabartinis rezultatas yra ${result}.</li>`
+    h1DOM.innerText = 'Show must go on!';
 })
